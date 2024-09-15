c16dc9243f443ab0863bc1219a22ba2bebe10631
Thomas Rampelberg <thomas@saunter.org>
2024-09-15 12:53:51 -0700

    docs: updating diagrams

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 
 import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import mermaid from 'mermaid'
 
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
@@ -14,6 +15,31 @@ if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG) {
     },
   })
 }
+
+mermaid.registerIconPacks([
+  {
+    name: 'logos',
+    loader: () => import('@iconify-json/logos').then((module) => module.icons),
+  },
+  {
+    name: 'line-md',
+    loader: () =>
+      import('@iconify-json/line-md').then((module) => module.icons),
+  },
+  {
+    name: 'carbon',
+    loader: () => import('@iconify-json/carbon').then((module) => module.icons),
+  },
+  {
+    name: 'material-symbols',
+    loader: () =>
+      import('@iconify-json/material-symbols').then((module) => module.icons),
+  },
+  {
+    name: 'mdi',
+    loader: () => import('@iconify-json/mdi').then((module) => module.icons),
+  },
+])
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
